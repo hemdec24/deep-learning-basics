6f0990765fb6e53de79f843f0fbe9d9b2f0e8805
Hemchand Ramireddy <hemchand.ramireddy@oracle.com>
2018-05-31 18:25:23 -0700

    first commit

---
+++ src/main/java/com/praja/dl/App.java
@@ -0,0 +1,13 @@
+package com.praja.dl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
